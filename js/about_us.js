35bcbcce236aedbcdc05c6b4f2d0fceeeb2b3523
ashmankumar <akum82@student.monash.edu>
2016-06-24 21:20:53 +1000

    Added map to contact us page

---
+++ js/about_us.js
@@ -0,0 +1,15 @@
+var map;
+function initMap() {
+var center = {lat: -34.283035, lng: 146.04701};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 14
+  });
+    
+  var marker = new google.maps.Marker({
+    position: center,
+    map: map,
+    title: 'Kindy Lane'
+  });
+}
+
